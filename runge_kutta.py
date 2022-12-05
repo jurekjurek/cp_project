aba724ff27b9ed99fe47bc0d40ff61d76c938c3a
Jurek <jurek.eisinger@t-online.de>
2022-12-05 15:53:51 +0100

    update

--- runge_kutta.py
+++ runge_kutta.py
@@ -21,8 +21,40 @@ B = 0.005 #L**2 / m**2
 C = 0.8 #3*G*M*L**2 / (m*c**2)
 
 # small r, high v are most intersting region for us 
-r0 = 2.1        #0.83 #+ 0.003
+r0 = 1
+
 v0 = 1
+
+
+
+
+
+
+
+# r0_31 = 1.9 #2.1        #0.83 #+ 0.003
+# r0_22 = 2.0
+# r0_13 = 2.1
+
+a = np.sqrt((0.1)**2/2)
+
+# r0_11 = np.sqrt((r0_31**2)+(2*a)**2)
+# r0_12 = np.sqrt((r0_22**2)+a**2)
+# r0_21 = np.sqrt((r0_31**2)+a**2)
+# r0_23 = np.sqrt((r0_22**2)+a**2)
+# r0_32 = np.sqrt((r0_31**2)+a**2) 
+# r0_33 = np.sqrt((r0_31**2)+(2*a)**2)
+
+
+r0_12 = 2.1
+r0_11 = np.sqrt((r0_12**2)+a**2)
+r0_13 = np.sqrt((r0_12**2)+a**2)
+r0_22 = 2.0
+r0_21 = np.sqrt((r0_22**2)+a**2)
+r0_23 = np.sqrt((r0_22**2)+a**2)
+r0_32 = 1.9
+r0_31 = np.sqrt((r0_32**2)+a**2)
+r0_33 = np.sqrt((r0_32**2)+a**2)
+
 
 # escape velocity for newtonian potential
 # v0 = np.sqrt(2/r0) 
@@ -55,8 +87,8 @@ Zweiter Versuch
 r = np.zeros(len(t))
 v = np.zeros(len(t))
 
-r[0] = r0
-v[0] = v0
+# r[0] = r0
+# v[0] = v0
 
 
 # def rk4(h = 0.05):
@@ -76,7 +108,7 @@ v[0] = v0
 #         # vn = v0 + kv
 #     return v, r
 
-def my_rk4(h = 0.2):
+def my_rk4(r0, v0, h = 0.2):
     r = np.zeros(len(t))
     v = np.zeros(len(t))
     r[0] = r0
@@ -90,7 +122,7 @@ def my_rk4(h = 0.2):
         k3v = (v_(t[i]+ h/2, r[i] + h*k2r/2, v[i] + h*k2v/2))
         k4r = (r_(t[i]+ h, r[i] + h*k3r/2, v[i] + h*k3v/2))
         k4v = (v_(t[i]+ h, r[i] + h*k3r/2, v[i] + h*k3v/2))
-        print(k1r, k2r, k3r, k4r)
+        # print(k1r, k2r, k3r, k4r)
         r[i+1]  = r[i] + (k1r+2*k2r+2*k3r+k4r)*h/6
         v[i+1]  = v[i] + (k1v+2*k2v+2*k3v+k4v)*h/6
         if r[i] <= 0:
@@ -104,20 +136,56 @@ def my_rk4(h = 0.2):
 # v_res, r_res = rk4()
 # print(np.shape(v_res), np.shape(r_res))
 
-my_v, my_r = my_rk4()
+my_v, my_r = my_rk4(r0, v0)
 print(len(my_v), len(my_r))
 
-plt.figure()
-plt.plot(t[:len(my_r)], my_r)
-plt.xlabel('Time')
-plt.ylabel('Solution of r(t)')
-plt.show()
+print(np.shape(my_r))
+
+r0_list = [r0_11, r0_12, r0_13,r0_21 ,r0_22,r0_23 , r0_31, r0_32, r0_33]
+
+r_list = np.zeros(9)
+v_list = np.zeros(9)
+r_testt = []
+
+v_11, r_11 = my_rk4(r0_11, v0)
+v_12, r_12 = my_rk4(r0_12, v0)
+v_13, r_13 = my_rk4(r0_13, v0)
+
+v_21, r_21 = my_rk4(r0_22, v0)
+v_22, r_22 = my_rk4(r0_22, v0)
+v_23, r_23 = my_rk4(r0_23, v0)
+
+v_31, r_31 = my_rk4(r0_31, v0)
+v_32, r_32 = my_rk4(r0_32, v0)
+v_33, r_33 = my_rk4(r0_33, v0)
+
+
+r_list.reshape(3,3)
+
+print(r0_list)
 
 plt.figure()
-plt.plot(t[:len(my_r)], my_v)
+plt.plot(t[:len(r_11)], r_11, label = 'r_11')
+plt.plot(t[:len(r_12)], r_12, label = 'r_12')
+plt.plot(t[:len(r_13)], r_13, label = 'r_13')
+
+plt.plot(t[:len(r_21)], r_21, label = 'r_21')
+plt.plot(t[:len(r_22)], r_22, label = 'r_22')
+plt.plot(t[:len(r_23)], r_23, label = 'r_23')
+
+plt.plot(t[:len(r_31)], r_31, label = 'r_31')
+plt.plot(t[:len(r_32)], r_32, label = 'r_32')
+plt.plot(t[:len(r_33)], r_33, label = 'r_33')
 plt.xlabel('Time')
-plt.ylabel('Solution of v(t)')
+plt.ylabel('Solution of r(t)')
+plt.legend()
 plt.show()
+
+# plt.figure()
+# plt.plot(t[:len(my_r)], my_v)
+# plt.xlabel('Time')
+# plt.ylabel('Solution of v(t)')
+# plt.show()
 
 
 
