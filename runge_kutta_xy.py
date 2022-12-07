2c7354e693b2cfc7848459d02a511d791e9ecd3f
Jurek <jurek.eisinger@t-online.de>
2022-12-07 12:23:43 +0100

    update. We now have a working potential for the black hole. If r smaller r isco we see that the orbits become unstable. Im not quite sure why though

--- runge_kutta_xy.py
+++ runge_kutta_xy.py
@@ -11,32 +11,104 @@ from scipy import constants
 G = constants.G
 c = constants.speed_of_light
 
-m = 1 #1.989 * 10**30              # mass of the sun in kg
-M = 2 #10**6 * m                   # mass of BH
+m = 1.989 * 10**30              # mass of the sun in kg
+M = 10**6 * m                   # mass of BH
 
-L = 100
+r_star = 1 
 
-A = 1 # G*M
-B = 0.005 #L**2 / m**2
-C = 0.8 #3*G*M*L**2 / (m*c**2)
+'''
+Initial conditions
+'''
 
+# we're gonna use kepler = 2 potential
+# as soon as r<r_isco the orbits become unstable
+
+x0 =    0
+y0 =    8*10**9     # close to the isco radius
+# v_x0 =  10**8
+v_y0 =  0
+
+# orbit velo
+# v_x0 = np.sqrt(G*M/y0)
+
+# orbit velo for BH
+# it actually works...
+v_x0 = np.sqrt(G*M/y0 + 3*G**2*M**2/(y0**2*c**2))
+
+# escape velo, A = G*M
+# we see that for 70% e.g. the particle falls back into the Mass
+# v_x0 = 0
+# v_y0 = np.sqrt(A*2/y0)*0.7
+
+print('vy_0 is ', v_x0/c*100, '% of the speed of light.')
+
+def L(x,y, m):
+    '''
+    function that calculates the angular momentum for given x, y
+    L is constant over time
+    Since at point t = 0, we have only a velocity component in x-direction, so we calculate the L(0) = L(t) to be:
+    '''
+    print(x,y)
+    x = float(x)
+    y = float(y)
+    r = np.sqrt((x**2+y**2))
+    return r*m*v_x0
+
+L = L(x0, y0, m)
+print(L)
+
+A = G*M
+B = L**2 / m**2             # 0.005
+C = 3*G*M*L**2 / (m*c**2)   # 0.8
+
+
+# things for the Black hole
+
+r_ss   = 2*A/c**2
+r_isco = 3*r_ss
+
+# stable orbit for photons
+r_photon = 1.5 * r_ss
+
+# tidal radius
+r_tidal = r_star * (M/m)**(1/3)
 
 ##############################################################################################################
 #                               Kepler orbit or BH orbit? 
-kepler = True
+kepler = 2              
+# set to True for Newtonian potential, False for BH potential, which does not seem to work at all
+# and to 2 for the alternative potential from the paper
 ##############################################################################################################
 
 
 # small r, high v are most intersting region for us 
-r0 = 1
 
-v0 = 0
+'''
+for the kepler potential we see that we still have an orbit for r<r_isco
+That makes total sense, there is no isco for keplerian potential
+these values give reasonable results for the keplerian potential: 
 
-x0 =    10
-y0 =    10
-v_x0 =  0
+m = 1.989 * 10**30              # mass of the sun in kg
+M = 10**6 * m                   # mass of BH
+
+r_star = 1 
+
+A = G*M
+
+
+# things for the Black hole
+
+r_ss   = 2*A/c**2
+r_isco = 3*r_ss
+
+x0 =    0
+y0 =    5*10**9    
 v_y0 =  0
 
+# orbit velo
+v_x0 = np.sqrt(A/y0)
+
+'''
 
 
 
@@ -47,38 +119,32 @@ v_y0 =  0
 # escape velocity for newtonian potential
 # v0 = np.sqrt(2/r0) 
 
-t = np.linspace(0,1000,1000)
+t = np.linspace(0,100000,10000)
 
 t0 = t[0]
 
 h = len(t)
 
-# function to be solved: dy/dx = x + y
-def f1(t,r,v):
-    return v
-
-def f2(t,r,v, k=1, m=1):
-    return (-k/m)*r
-
-# du/dx = f(x)
-def v_(t,r,v):
-    # r = np.sqrt(x**2+y**2)
-    if kepler == True:
-        return -A/r**2 #+ B/(r**3)-C/(r**4)
-    else:
-        return -A/r**2 + B/(r**3)-C/(r**4)
+# as proposed in the paper: 
+# https://arxiv.org/pdf/2008.04922.pdf
 
 def v_x_(t,x,y,v_x,v_y):
     r = np.sqrt((x**2+y**2))
     if kepler == True:
         return -A*x / r**3 #+ B*x / r**4 - C*x / r**5
+    elif kepler == 2:
+        return -A*x / r**3 - 3*(G*M/c)**2*x / r**4
     else:
         return -A*x / r**3 + B*x / r**4 - C*x / r**5
 
 def v_y_(t,x,y,v_x,v_y):
     r = np.sqrt((x**2+y**2))
-    return -A*y / r**3 + B*y / r**4 - C*y / r**5
-
+    if kepler == True:
+        return -A*y / r**3 #+ B*y / r**4 - C*y / r**5
+    elif kepler == 2:
+        return -A*y / r**3 - 3*(G*M/c)**2*y / r**4
+    else: 
+        return -A*y / r**3 + B*y / r**4 - C*y / r**5
 # this acts as v = dot(x) and v = dot(y)
 def x_(t,x,y,v_x,v_y):
     return v_x
@@ -86,9 +152,9 @@ def x_(t,x,y,v_x,v_y):
 def y_(t,x,y,v_x,v_y):
     return v_y
 
-'''
-Zweiter Versuch
-'''
+
+
+
 
 def my_rk4(h = 0.2):
     '''
@@ -133,7 +199,7 @@ def my_rk4(h = 0.2):
 
 
         # define black hole radius to be one right now
-        if np.sqrt(x[i]**2+y[i]**2) <= 1:
+        if np.sqrt(x[i]**2+y[i]**2) <= 10**(-17):
             x[i] = 0
             y[i] = 0
             x = x[:i]
@@ -152,21 +218,32 @@ my_r = np.sqrt(my_x**2+ my_y**2)
 
 
 plt.figure()
-plt.plot(my_x, my_y, label = 'r_11')
+plt.plot(my_x, my_y)
 
 plt.xlabel('X-coordinate of the particle')
 plt.ylabel('Y-coordinate of the particle')
+plt.scatter(my_x[0], my_y[0], label = 'beginning')
+plt.scatter(my_x[-1], my_y[-1], label = 'end')
+plt.scatter(0,0, label= 'Black Hole', color = 'black')#, s = 300)
+plt.scatter(0,-r_isco, label = 'isco radius')
+plt.scatter(0,-r_ss, label = 'Schwarzschild radius')
+# if we maybe want to do it more fancy:
+#
+# plt.Circle((0,0), 10, color = 'black', fill = False, lw = 2)
+
+plt.xlim(-y0*2, y0*2)
+plt.ylim(-y0*2, y0*2)
 plt.title('Motion of the particle in the Kepler field - solved with RK4')
-plt.legend()
+plt.legend(loc = 'upper right')
 plt.show()
 
-plt.figure()
-plt.plot(t[:len(my_x)], my_r)
-plt.xlabel('Time')
-plt.ylabel('Solution of r(t)')
-plt.legend()
-plt.title('r(t) = $\sqrt{x(t)^2+y(t)^2}$ over time')
-plt.show()
+# plt.figure()
+# plt.plot(t[:len(my_x)], my_r)
+# plt.xlabel('Time')
+# plt.ylabel('Solution of r(t)')
+# plt.legend()
+# plt.title('r(t) = $\sqrt{x(t)^2+y(t)^2}$ over time')
+# plt.show()
 
 # plt.figure()
 # plt.plot(t[:len(my_r)], my_v)
