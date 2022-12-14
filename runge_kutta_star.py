f147a6443aa93d719ce3b8cc75409f14afd7045f
Jurek <jurek.eisinger@t-online.de>
2022-12-14 19:55:46 +0100

    a

--- runge_kutta_star.py
+++ runge_kutta_star.py
@@ -21,8 +21,6 @@ r_star = 696340*10**3
 Initial conditions
 '''
 
-# we're gonna use kepler = 2 potential
-# as soon as r<r_isco the orbits become unstable
 # tidal radius
 r_tidal = r_star * (M/m)**(1/3)
 
@@ -31,7 +29,7 @@ y0 =    1.2*r_tidal #8*10**9     # close to the isco radius
 # v_x0 =  10**8
 v_y0 =  0
 
-# orbit velo
+# orbit velo for kepler potential
 # v_x0 = np.sqrt(G*M/y0)
 
 # orbit velo for BH
@@ -83,35 +81,6 @@ kepler = 2
 ##############################################################################################################
 
 
-# small r, high v are most intersting region for us 
-
-'''
-for the kepler potential we see that we still have an orbit for r<r_isco
-That makes total sense, there is no isco for keplerian potential
-these values give reasonable results for the keplerian potential: 
-
-m = 1.989 * 10**30              # mass of the sun in kg
-M = 10**6 * m                   # mass of BH
-
-r_star = 1 
-
-A = G*M
-
-
-# things for the Black hole
-
-r_ss   = 2*A/c**2
-r_isco = 3*r_ss
-
-x0 =    0
-y0 =    5*10**9    
-v_y0 =  0
-
-# orbit velo
-v_x0 = np.sqrt(A/y0)
-
-'''
-
 
 
 
@@ -124,192 +93,6 @@ t = np.linspace(0,100000,10000)
 t0 = t[0]
 
 h = len(t)
-
-def r_help(A, B, C, r_i):
-    '''
-    C is the force on the ith particle
-    Solves -A/r^2 - B/r^3 = -C for r
-    '''
-
-    r = symbols('r')
-    return solve(-A/((r-r_i)**2)-B/((r-r_i)**3)+C, r)
-
-    r_thr =  ((2/3)**(1/3) * A)/(np.sqrt(3) * (27 * B**2 * C**4 - 4 * A**3 * C**3)**(1/2) + 9 * B * C**2)**(1/3) + (np.sqrt(3) * (27 * B**2 * C**4 - 4 * A**3 * C**3)**(1/2) + 9 * B * C**2)**(1/3)/(2**(1/3) * 3**(2/3) * C)
-
-    return r_thr
-
-def r_help_bh(D):
-    A = G*M*m/5
-    B = L**2 / (4*m**2) * m/5             # 0.005
-    C = 3*G*M*L**2 / (m*c**2) * m/5   # 0.8
-
-    r = symbols('r')
-    return solve(-A / r**2 + B / r**3 - C / r**4 + D, r)
-
-
-# This is the best try so far
-# for F5, we assume the outer layer is bound by the whole self grav, the i-1th layer bound by 4/5*m**2 and so on 
-# we get 4*the tidal radius... which is still not good 
-
-# def grav_layer(r_star, m_star, split = 5):
-#     '''
-#     Calculate the different radii for the different layers
-#     assuming constant density
-#     '''
-#     m = m_star 
-#     # r1 = r_star / (split**(1/3)) 
-#     # r1 = r1
-#     # r2 = (2**(1/3)-1)*r1
-#     # r3 = (3**(1/3)-2**(1/3))*r1
-#     # r4 = (4**(1/3)-3**(1/3))*r1
-#     # r5 = r_star
-
-#     r1 = r_star / (split**(1/3)) 
-#     r1 = r1
-#     r2 = (2**(1/3)-1)*r1 #+ r1
-#     r3 = (3**(1/3)-2**(1/3))*r1 #+ r1 + r2 
-#     r4 = (4**(1/3)-3**(1/3))*r1 #+ r1 + r2 +r3
-#     r5 = (5**(1/3)-4**(1/3))*r1 #+ r1 + r2 +r3 + r4  # this is the rad of the star
-    
-    
-#     print('should reproduce whole star radius...: ', (r1+r2+r3+r4+r5)/100000, r_star/100000)
-    
-#     r1_tot = r1
-#     r2_tot = r2 + r1
-#     r3_tot = r3+r2+r1
-#     r4_tot = r4+r3+r2+r1
-
-
-#     F5 = G*m**2/(((r3_tot+r4_tot)/2)**2)
-
-#     F4 = G*(4/5*m**2)/(((r3_tot+r2_tot)/2)**2)
-#     F3 = G*(3/5*m**2)/(((r2_tot+r1_tot)/2)**2)
-#     F2 = G*(2/5*m**2)/(((r1_tot+0)/2)**2)
-
-#     print('radii 2,3,4,5: ', r1/10000,r2/10000,r3/10000,r4/10000)
-
-#     print('Forces ascending: ',F2, F3, F4, F5)
-
-
-#     A5 = G*M*m*1/5
-#     A4 = G*M*m*1/5
-#     A3 = G*M*m*1/5
-#     A2 = G*M*m*1/5
-
-#     B5 = 3*(G*M/c)**2*m*1/5
-#     B4 = 3*(G*M/c)**2*m*1/5
-#     B3 = 3*(G*M/c)**2*m*1/5
-#     B2 = 3*(G*M/c)**2*m*1/5
-
-
-#     r2_thr = np.array([complex(item) for item in r_help(A2,B2,F2, r2_tot)])[2].real
-#     r3_thr = np.array([complex(item) for item in r_help(A3,B3,F3, r3_tot)])[2].real
-#     r4_thr = np.array([complex(item) for item in r_help(A4,B4,F4, r3_tot)])[2].real
-#     r5_thr = np.array([complex(item) for item in r_help(A5,B5,F5, r3_tot)])[2].real 
- 
-#     # eq to solve: -A/r^2 - B/r^3 = -F_i for r
-#     # return F2,F3,F4,F5
-#     r1_ = r1_tot/2
-#     r2_ = (r1_tot+r2_tot)/2
-#     r3_ = (r2_tot+r3_tot)/2
-#     r4_ = (r3_tot+r4_tot)/2
-#     r5_ = (r4_tot+r_star)/2
-#     return np.array([r1_, r2_, r3_, r4_, r5_])
-#     return r2_thr, r3_thr, r4_thr, r5_thr
-
-
-
-
-def grav_layer(r_star, m_star, split = 5):
-    '''
-    Calculate the different radii for the different layers
-    assuming constant density
-    '''
-    m = m_star 
-    # r1 = r_star / (split**(1/3)) 
-    # r1 = r1
-    # r2 = (2**(1/3)-1)*r1
-    # r3 = (3**(1/3)-2**(1/3))*r1
-    # r4 = (4**(1/3)-3**(1/3))*r1
-    # r5 = r_star
-
-    # r1 = r_star / (split**(1/3)) 
-    # r1 = r1
-    # r2 = (2**(1/3)-1)*r1 #+ r1
-    # r3 = (3**(1/3)-2**(1/3))*r1 #+ r1 + r2 
-    # r4 = (4**(1/3)-3**(1/3))*r1 #+ r1 + r2 +r3
-    # r5 = (5**(1/3)-4**(1/3))*r1 #+ r1 + r2 +r3 + r4  # this is the rad of the star
-    
-    r1 = r_star / 5
-    r2 = r_star / 5 
-    r3 = r_star / 5 
-    r4 = r_star / 5 
-    r5 = r_star / 5 
-    
-    
-    print('should reproduce whole star radius...: ', (r1+r2+r3+r4+r5)/100000, r_star/100000)
-    
-    r1_tot = r1
-    r2_tot = r2 + r1
-    r3_tot = r3+r2+r1
-    r4_tot = r4+r3+r2+r1
-
-    F5 = G*m**2/(((r4_tot+r_star)/2)**2)
-    F4 = G*(4/5*m**2)/(((r3_tot+r4_tot)/2)**2)
-
-    F3 = G*(3/5*m**2)/(((r3_tot+r2_tot)/2)**2)
-    F2 = G*(2/5*m**2)/(((r2_tot+r1_tot)/2)**2)
-    F1 = G*(1/5*m**2)/(((r1_tot+0)/2)**2)
-
-
-    # F_test = G
-
-    # F5 = G*1/5*m*(m)/(((r4_tot+r_star)/2)**2)
-    # F4 = G*1/5*m*(4/5*m)/(((r3_tot+r4_tot)/2)**2)
-
-    # F3 = G*1/5*m*(3/5*m)/(((r3_tot+r2_tot)/2)**2)
-    # F2 = G*1/5*m*(2/5*m)/(((r2_tot+r1_tot)/2)**2)
-    # F1 = G*1/5*m*(1/5*m)/(((r1_tot+0)/2)**2)
-
-    print('radii 2,3,4,5: ', r1/10000,r2/10000,r3/10000,r4/10000)
-
-    print('Forces ascending: ',F2, F3, F4, F5)
-
-
-    A5 = G*M*m*1/5
-    A4 = G*M*m*1/5
-    A3 = G*M*m*1/5
-    A2 = G*M*m*1/5
-
-    B5 = 3*(G*M/c)**2*m*1/5
-    B4 = 3*(G*M/c)**2*m*1/5
-    B3 = 3*(G*M/c)**2*m*1/5
-    B2 = 3*(G*M/c)**2*m*1/5
-
-    r1_thr = np.array([complex(item) for item in r_help(A2,B2,F1, r1_tot)])[2].real
-    r2_thr = np.array([complex(item) for item in r_help(A2,B2,F2, r2_tot)])[2].real
-    r3_thr = np.array([complex(item) for item in r_help(A3,B3,F3, r3_tot)])[2].real
-    r4_thr = np.array([complex(item) for item in r_help(A4,B4,F4, r4_tot)])[2].real
-    r5_thr = np.array([complex(item) for item in r_help(A5,B5,F5, r_star)])[2].real 
- 
-    # eq to solve: -A/r^2 - B/r^3 = -F_i for r
-    # return F2,F3,F4,F5
-    return r1_thr, r2_thr, r3_thr, r4_thr, r5_thr
-
-
-'''
-Keine Konstante Density annehmen!!!!! 
-So funktioniert es für den Tidal Radius, guck nachher weiter!!!!
-'''
-
-# r1, r2,r3,r4,r5 = grav_layer(r_star, m)
-
-# print(r2, r3, r4, r5)
-# print(r1/r_tidal, r2/r_tidal, r3/r_tidal, r4/r_tidal, r5/r_tidal)
-
-
-
-
 
 
 # as proposed in the paper: 
@@ -350,9 +133,12 @@ def my_rk4(x0, v_x0, y0, v_y0, star, h = 0.2):
     for the x- and y-component of a particle. At each step, we evaluate v[i] for x and y which corresponds to their derivatives 
     as well as the derivatives dot(v) for x and y which corresponds to their second order derivatives. 
     Since for the (i+1)-th step, we need the values of x, y, v_x and v_y for the i-th step, for each step we calculate these 4 values
-    star is a variable that tells us what kind of object we are solving the eom for
+    star is a variable that tells us what kind of object we are solving the eom for, 
+    star = True: The star that further splits into particles
+    star = False: One particle the star turned into
     '''
     # F1,F2,F3,F4 = grav_layer(r_star, m)
+    print('#')
     x = np.zeros(len(t))
     v_x = np.zeros(len(t))
     y = np.zeros(len(t))
@@ -401,10 +187,11 @@ def my_rk4(x0, v_x0, y0, v_y0, star, h = 0.2):
         if np.sqrt(x[i]**2+y[i]**2) <= r_tidal:
             x[i] = 0
             y[i] = 0
-            x = x[:i]
-            y = y[:i]
-            v_x = v_x[:i]
-            v_y = v_y[:i]
+            # sets the valus after this time to zero, since it's not going to evolve anymore
+            # x = x[:i]
+            # y = y[:i]
+            # v_x = v_x[:i]
+            # v_y = v_y[:i]
             break
 
         # if we want the eom for the particles, we do not want to divide them up further into more particles
@@ -469,18 +256,17 @@ def my_rk4(x0, v_x0, y0, v_y0, star, h = 0.2):
 
 # creates 8 particles for each layer in the star 
 
-def particles(r_layer, x_star, y_star, vx_star, vy_star, number_of_particles=8):
+def particles(r_layer, x_star, y_star, number_of_particles=8):
     '''
+    This function creates initial positions for the particles once the shell splits up
+    This initial position is dependent on the position of the star (x_star, y_star)
     All particles have the same velocity but different initial positions
     '''
-    vx0 = vx_star
-    vy0 = vy_star
-
 
     x_is = np.zeros(number_of_particles)
     y_is = np.zeros(number_of_particles)
 
-    # for every one of the 9 particles in the j-th shell define the initial positions
+    # for every one of the 8 particles in the j-th shell define the initial positions
     x_is[0] = 0
     y_is[0] = 1
 
@@ -508,18 +294,13 @@ def particles(r_layer, x_star, y_star, vx_star, vy_star, number_of_particles=8):
     x_is = x_is * r_layer
     y_is = y_is * r_layer
 
-    x_is = x_is + x_star
-    y_is = y_is + y_star 
-
-    # for i in range(number_of_particles):
-    #     x_is[i] = x_star + x_is[i]
-    #     y_is[i] = y_star + y_is[i]    
+    x_is = x_is + x_star    # adding the constant value x_star to the array x_is 
+                            # every particle has an initial position relative to the star, then we add the star position to get the global initial position
+    y_is = y_is + y_star    
 
     return x_is, y_is
 
-# x_is, y_is = particles(r5_layer, x0, y0, v_x0, v_y0) 
-# print(r5_layer)
-# print(x_is, y_is)
+
 
 def execute():
 
@@ -531,18 +312,8 @@ def execute():
 
     number_of_layers = 5
     number_of_particles = 8
-    # x_is_1 = np.zeros(number_of_particles)
-    # x_is_2 = np.zeros(number_of_particles)
-    # x_is_3 = np.zeros(number_of_particles)
-    # x_is_4 = np.zeros(number_of_particles)
-    # x_is_5 = np.zeros(number_of_particles)
-    # x_is = np.array([x_is_1, x_is_2, x_is_3, x_is_4, x_is_5])
 
-    # x_is and y_is will consist of the initial conditions (x and y) for every particle in every layer
-    x_is = np.zeros((number_of_layers, number_of_particles))
-    y_is = x_is
-    # r_layer_list = grav_layer(r_star, m)
-    
+
 
     # we get x_star, y_star, vx_star ... from calling rk 4 for the star 
     # but! These will also be 1-dim np.arrays with length 5 (for every layer)
@@ -555,15 +326,20 @@ def execute():
     print('####################################')
 
     '''
-    Here we create the matrices with the initial conditions 
+    Create the matrices with the initial conditions 
     '''
+    # x_is and y_is will consist of the initial conditions (x and y) for every particle in every layer
+    x_is = np.zeros((number_of_layers, number_of_particles))
+    y_is = x_is
 
-    # if every star is divided in 5 equal radii
+    # if every star is divided in 5 equal radii, r_layer_list contains the radius for every shell, r[0] for 1st shell, r[1] for 2nd and so on...
     a = r_star/5
     r_layer_list = np.array([a/2, 3/2*a, 5/2*a, 7/2*a, 9/2*a])
     for i in range(number_of_layers):
         # in every layer we have 8 particles created 
-        x_is_, y_is_ = particles(r_layer_list[i], x_star[i], y_star[i], vx_star[i], vy_star[i]) 
+        # the positions where the particles are created are dependent on the radius of the shell and 
+        # the position of the star at the point where the shell splits up
+        x_is_, y_is_ = particles(r_layer_list[i], x_star[i], y_star[i])         # initial positions for particles in i-th layer 
         for j in range(number_of_particles):
         # iterate over these 8 particles and fill the x_is with the initial conditions 
         # for the i-th layer we create j particles
@@ -572,7 +348,7 @@ def execute():
 
 
     '''
-    Here the eom for the particles are solved with the respective initial conditions 
+    Eom for the particles are solved with the respective initial conditions 
     '''
 
     # here I solve the eom for the particles (for all 40)
@@ -588,14 +364,15 @@ def execute():
             # solve the eom and save the trajectories x and y in x_final and y_final
             # where x_is and y_is are the initial conditions for the [i,j]th particle
             print(np.shape(x_final[i,j,:]), np.shape(y_final), np.shape(x_is), np.shape(y_is), np.shape(vx_star[i]) ,np.shape(vy_star))
-            my_x_final, my_y_final, my_vx_final, my_vy_final  = my_rk4(x_is[i,j], y_is[i,j], vx_star[i], vy_star[i], star=False)
-            print(np.shape(my_x_final))
+            x_final[i,j,:], y_final[i,j,:], vx_final[i,j,:], vy_final[i,j,:] = my_rk4(x_is[i,j], y_is[i,j], vx_star[i], vy_star[i], star=False)
+            # print(np.shape(my_x_final), np.shape(my_y_final))
 
 
     # so now, we should have: 
     # 40 eoms for 40 particles, all movement in x_final and y_final
     # and 1 eom of the star. The star disappears at some point. 
     
-    return x_star, y_star, vx_star, vy_star, x_final, y_final
+    return my_x, my_y, my_vx, my_vy, x_final, y_final, vx_final, vy_final
 
-execute()
+x, y, vx, vy, x_p, y_p, vx_p, vy_p = execute()
+print(np.shape(x), np.shape(y), np.shape(vx), np.shape(vy), np.shape(x_p), np.shape(y_p), np.shape(vx_p), np.shape(vy_p))
--- trash.py
+++ trash.py
@@ -137,3 +137,237 @@
     x5_8, y5_8, vx5_8, vy5_8 = my_rk4(x_is[4,7], y_is[4,7],vx_star, vy_star,star=False)
 
 
+    # x_is_1 = np.zeros(number_of_particles)
+    # x_is_2 = np.zeros(number_of_particles)
+    # x_is_3 = np.zeros(number_of_particles)
+    # x_is_4 = np.zeros(number_of_particles)
+    # x_is_5 = np.zeros(number_of_particles)
+    # x_is = np.array([x_is_1, x_is_2, x_is_3, x_is_4, x_is_5])
+
+
+
+
+
+
+###
+# This is the grav_layer function, so not really trash. We can use it later maybe to get closer to the actual r_tidal
+###
+
+def r_help(A, B, C, r_i):
+    '''
+    C is the force on the ith particle
+    Solves -A/r^2 - B/r^3 = -C for r
+    '''
+
+    r = symbols('r')
+    return solve(-A/((r-r_i)**2)-B/((r-r_i)**3)+C, r)
+
+    r_thr =  ((2/3)**(1/3) * A)/(np.sqrt(3) * (27 * B**2 * C**4 - 4 * A**3 * C**3)**(1/2) + 9 * B * C**2)**(1/3) + (np.sqrt(3) * (27 * B**2 * C**4 - 4 * A**3 * C**3)**(1/2) + 9 * B * C**2)**(1/3)/(2**(1/3) * 3**(2/3) * C)
+
+    return r_thr
+
+def r_help_bh(D):
+    A = G*M*m/5
+    B = L**2 / (4*m**2) * m/5             # 0.005
+    C = 3*G*M*L**2 / (m*c**2) * m/5   # 0.8
+
+    r = symbols('r')
+    return solve(-A / r**2 + B / r**3 - C / r**4 + D, r)
+
+
+# This is the best try so far
+# for F5, we assume the outer layer is bound by the whole self grav, the i-1th layer bound by 4/5*m**2 and so on 
+# we get 4*the tidal radius... which is still not good 
+
+# def grav_layer(r_star, m_star, split = 5):
+#     '''
+#     Calculate the different radii for the different layers
+#     assuming constant density
+#     '''
+#     m = m_star 
+#     # r1 = r_star / (split**(1/3)) 
+#     # r1 = r1
+#     # r2 = (2**(1/3)-1)*r1
+#     # r3 = (3**(1/3)-2**(1/3))*r1
+#     # r4 = (4**(1/3)-3**(1/3))*r1
+#     # r5 = r_star
+
+#     r1 = r_star / (split**(1/3)) 
+#     r1 = r1
+#     r2 = (2**(1/3)-1)*r1 #+ r1
+#     r3 = (3**(1/3)-2**(1/3))*r1 #+ r1 + r2 
+#     r4 = (4**(1/3)-3**(1/3))*r1 #+ r1 + r2 +r3
+#     r5 = (5**(1/3)-4**(1/3))*r1 #+ r1 + r2 +r3 + r4  # this is the rad of the star
+    
+    
+#     print('should reproduce whole star radius...: ', (r1+r2+r3+r4+r5)/100000, r_star/100000)
+    
+#     r1_tot = r1
+#     r2_tot = r2 + r1
+#     r3_tot = r3+r2+r1
+#     r4_tot = r4+r3+r2+r1
+
+
+#     F5 = G*m**2/(((r3_tot+r4_tot)/2)**2)
+
+#     F4 = G*(4/5*m**2)/(((r3_tot+r2_tot)/2)**2)
+#     F3 = G*(3/5*m**2)/(((r2_tot+r1_tot)/2)**2)
+#     F2 = G*(2/5*m**2)/(((r1_tot+0)/2)**2)
+
+#     print('radii 2,3,4,5: ', r1/10000,r2/10000,r3/10000,r4/10000)
+
+#     print('Forces ascending: ',F2, F3, F4, F5)
+
+
+#     A5 = G*M*m*1/5
+#     A4 = G*M*m*1/5
+#     A3 = G*M*m*1/5
+#     A2 = G*M*m*1/5
+
+#     B5 = 3*(G*M/c)**2*m*1/5
+#     B4 = 3*(G*M/c)**2*m*1/5
+#     B3 = 3*(G*M/c)**2*m*1/5
+#     B2 = 3*(G*M/c)**2*m*1/5
+
+
+#     r2_thr = np.array([complex(item) for item in r_help(A2,B2,F2, r2_tot)])[2].real
+#     r3_thr = np.array([complex(item) for item in r_help(A3,B3,F3, r3_tot)])[2].real
+#     r4_thr = np.array([complex(item) for item in r_help(A4,B4,F4, r3_tot)])[2].real
+#     r5_thr = np.array([complex(item) for item in r_help(A5,B5,F5, r3_tot)])[2].real 
+ 
+#     # eq to solve: -A/r^2 - B/r^3 = -F_i for r
+#     # return F2,F3,F4,F5
+#     r1_ = r1_tot/2
+#     r2_ = (r1_tot+r2_tot)/2
+#     r3_ = (r2_tot+r3_tot)/2
+#     r4_ = (r3_tot+r4_tot)/2
+#     r5_ = (r4_tot+r_star)/2
+#     return np.array([r1_, r2_, r3_, r4_, r5_])
+#     return r2_thr, r3_thr, r4_thr, r5_thr
+
+
+# this is the function that kinda works, not assuming constant density!!
+
+# def grav_layer(r_star, m_star, split = 5):
+#     '''
+#     Calculate the different radii for the different layers
+#     assuming constant density
+#     '''
+#     m = m_star 
+#     # r1 = r_star / (split**(1/3)) 
+#     # r1 = r1
+#     # r2 = (2**(1/3)-1)*r1
+#     # r3 = (3**(1/3)-2**(1/3))*r1
+#     # r4 = (4**(1/3)-3**(1/3))*r1
+#     # r5 = r_star
+
+#     # r1 = r_star / (split**(1/3)) 
+#     # r1 = r1
+#     # r2 = (2**(1/3)-1)*r1 #+ r1
+#     # r3 = (3**(1/3)-2**(1/3))*r1 #+ r1 + r2 
+#     # r4 = (4**(1/3)-3**(1/3))*r1 #+ r1 + r2 +r3
+#     # r5 = (5**(1/3)-4**(1/3))*r1 #+ r1 + r2 +r3 + r4  # this is the rad of the star
+    
+#     r1 = r_star / 5
+#     r2 = r_star / 5 
+#     r3 = r_star / 5 
+#     r4 = r_star / 5 
+#     r5 = r_star / 5 
+    
+    
+#     print('should reproduce whole star radius...: ', (r1+r2+r3+r4+r5)/100000, r_star/100000)
+    
+#     r1_tot = r1
+#     r2_tot = r2 + r1
+#     r3_tot = r3+r2+r1
+#     r4_tot = r4+r3+r2+r1
+
+#     F5 = G*m**2/(((r4_tot+r_star)/2)**2)
+#     F4 = G*(4/5*m**2)/(((r3_tot+r4_tot)/2)**2)
+
+#     F3 = G*(3/5*m**2)/(((r3_tot+r2_tot)/2)**2)
+#     F2 = G*(2/5*m**2)/(((r2_tot+r1_tot)/2)**2)
+#     F1 = G*(1/5*m**2)/(((r1_tot+0)/2)**2)
+
+
+#     # F_test = G
+
+#     # F5 = G*1/5*m*(m)/(((r4_tot+r_star)/2)**2)
+#     # F4 = G*1/5*m*(4/5*m)/(((r3_tot+r4_tot)/2)**2)
+
+#     # F3 = G*1/5*m*(3/5*m)/(((r3_tot+r2_tot)/2)**2)
+#     # F2 = G*1/5*m*(2/5*m)/(((r2_tot+r1_tot)/2)**2)
+#     # F1 = G*1/5*m*(1/5*m)/(((r1_tot+0)/2)**2)
+
+#     print('radii 2,3,4,5: ', r1/10000,r2/10000,r3/10000,r4/10000)
+
+#     print('Forces ascending: ',F2, F3, F4, F5)
+
+
+#     A5 = G*M*m*1/5
+#     A4 = G*M*m*1/5
+#     A3 = G*M*m*1/5
+#     A2 = G*M*m*1/5
+
+#     B5 = 3*(G*M/c)**2*m*1/5
+#     B4 = 3*(G*M/c)**2*m*1/5
+#     B3 = 3*(G*M/c)**2*m*1/5
+#     B2 = 3*(G*M/c)**2*m*1/5
+
+#     r1_thr = np.array([complex(item) for item in r_help(A2,B2,F1, r1_tot)])[2].real
+#     r2_thr = np.array([complex(item) for item in r_help(A2,B2,F2, r2_tot)])[2].real
+#     r3_thr = np.array([complex(item) for item in r_help(A3,B3,F3, r3_tot)])[2].real
+#     r4_thr = np.array([complex(item) for item in r_help(A4,B4,F4, r4_tot)])[2].real
+#     r5_thr = np.array([complex(item) for item in r_help(A5,B5,F5, r_star)])[2].real 
+ 
+#     # eq to solve: -A/r^2 - B/r^3 = -F_i for r
+#     # return F2,F3,F4,F5
+#     return r1_thr, r2_thr, r3_thr, r4_thr, r5_thr
+
+
+'''
+Don't assume constant density probably
+'''
+
+# r1, r2,r3,r4,r5 = grav_layer(r_star, m)
+
+# print(r2, r3, r4, r5)
+# print(r1/r_tidal, r2/r_tidal, r3/r_tidal, r4/r_tidal, r5/r_tidal)
+
+
+###
+# This whole grav_layer stuff stops here
+###
+
+
+###
+# Reasonable results for kepler potential:
+###
+
+'''
+for the kepler potential we see that we still have an orbit for r<r_isco
+That makes total sense, there is no isco for keplerian potential
+these values give reasonable results for the keplerian potential: 
+
+m = 1.989 * 10**30              # mass of the sun in kg
+M = 10**6 * m                   # mass of BH
+
+r_star = 1 
+
+A = G*M
+
+
+# things for the Black hole
+
+r_ss   = 2*A/c**2
+r_isco = 3*r_ss
+
+x0 =    0
+y0 =    5*10**9    
+v_y0 =  0
+
+# orbit velo
+v_x0 = np.sqrt(A/y0)
+
+'''
+
