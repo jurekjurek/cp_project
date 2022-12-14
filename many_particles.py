2692b6de420d77bdb4327018d0189142ffdb5977
Jurek <jurek.eisinger@t-online.de>
2022-12-14 11:24:58 +0100

    a

--- many_particles.py
+++ many_particles.py
@@ -5,7 +5,7 @@ write a function that creates N particles out of i-th shell of star
 '''
 
 
-def particles(layer, x_star, y_star, vx_star, vy_star, number_of_particles=5):
+def particles(r_layer, x_star, y_star, vx_star, vy_star, number_of_particles=8):
     '''
     All particles have the same velocity but different initial positions
     '''
@@ -15,18 +15,41 @@ def particles(layer, x_star, y_star, vx_star, vy_star, number_of_particles=5):
     # the radii of the different particles relative to the center of the star 
     r4,r3,r2,r1 = grav_layer(r_star, m_star)
 
-    # x10 = 
-    # x20 = 
-    # x30 = 
-    # x40 = 
-    # x50 = 
+    x_is = np.zeros(number_of_particles)
+    y_is = np.zeros(number_of_particles)
 
-    # y10 = 
-    # y20 = 
-    # y30 = 
-    # y40 = 
-    # y50 = 
+    # for every one of the 9 particles in the j-th shell define the initial positions
+    x_is[0] = 0
+    y_is[0] = 1
 
+    x_is[1] = 1/(np.sqrt(2))
+    y_is[1] = 1/(np.sqrt(2))
+
+    x_is[2] = 1
+    y_is[2] = 0
+
+    x_is[3] = 1/(np.sqrt(2))
+    y_is[4] = -1/(np.sqrt(2))
+
+    x_is[4] = 0
+    y_is[4] = -1
+
+    x_is[5] = -1/(np.sqrt(2))
+    y_is[5] = -1/(np.sqrt(2))
+
+    x_is[6] = -1
+    y_is[6] = 0
+
+    x_is[7] = -1/(np.sqrt(2))
+    y_is[7] = 1/(np.sqrt(2))
+
+    x_is = x_is * r_layer
+    y_is = y_is * r_layer
+
+    for i in range(number_of_particles):
+        x_is[i] = x_star + x_is[i]
+        y_is[i] = y_star + y_is[i]
+        
 
     # for i in range(number_of_particles):
 
