5a20725bafb9be3454d99ba19bc1dd11f57bfa2f
jurekhackerman123 <jurek.eisinger@t-online.de>
2023-01-22 14:24:29 +0100

    a

--- signal_processing/filter_assignment_jurek_eisinger.py
+++ signal_processing/filter_assignment_jurek_eisinger.py
@@ -1,47 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f=200*10**6
-T=100*10**(-6)
-f_cutoff=48*10**6
-n=int(T*f)
-
-t=np.linspace(0,T,n)
-signal=np.sin(2*np.pi*f*t)
-for i in range(n):
-    signal[i]+=np.random.normal(0,1)
-
-
-
-
-
-# Generate random noise with a sampling frequency of 200 MHz and a total length of 100 microseconds.
+'''
+In this last assignment we implement lowpassfilters of two different lengths, convolve the corresponding impulse response functions 
+with random noise and fourier transform the signal to see the effects of the lowpass filter on the frequency distribution of the signal.
+'''
 
 
 def random_values(sampling_f, total_length):
+    '''
+    this function generates random values between -1 and 1 with a sampling frequency of sampling_f, 
+    and a total length of total_length
+    '''
     length = int(sampling_f*total_length)
     noise = np.random.random(length)*2-1        # generate random values between -1 and 1
     return noise
 
-noise = random_values(200*10**5, 0.1)
-print(np.shape(noise))
-print(noise[-1])
+# generate random noise with given sampling frequency (200 MHz) and total length (0.1s)
+noise = random_values(200*10**6, 0.1)
 
-# check if noise makes sense
-# works on Macos, the plot does not seem to work on windows, because it's too large
-x = np.linspace(0, 1000, len(noise))
-
-plt.figure()
-plt.plot(noise)
-plt.show()
-
-
-# Implement a low-pass filter of length M=50 with a cutoff frequency of 48 MHz. Calculate the impulse response of the filter.
-# For a low pass filter, I have to find a h(n) whose Fourier-trafo approximates this desired Freuquency response below(with a cutoff at 48 MHz)
 
 def impulse_response(M):
     '''
-    this function calculates the impulse resonse for a given filter length M accroding to the formula derived in class
+    this function calculates the impulse resonse for a given filter length M according to the formula derived in class
     '''
     N=M+1
     # define the step function responsible for letting nothing over a given frequency pass
@@ -56,18 +37,16 @@ def impulse_response(M):
         for j in range(1,N):
             h_sum[i] += A[j]*np.exp(-z*np.pi*j*((M-2*i)/N)).real        # ith element of h is sum over all 
         impulse_response[i] = (1/(N) * (A[0] + 3*h_sum[i]))
-        
-    # print(impulse_response)
-    return impulse_response/np.max(impulse_response)#np.sum(impulse_response)
+    return impulse_response/np.max(impulse_response)                    # return the normalized impulse response
 
-# Apply the impulse response to your generated noise.
 def main(signal = noise):
     '''
     This function generates the plots of the impulse functions calculated above. 
     The fouriertransformation of the with the impulse response convolved signal is being carried out and plotted 
     for both filter lengths; 50 and 200. 
     We see that the filter of length 200 produces a frequency sprectrum that oscillates more rapidly, thus converging 
-    (in the low-pass-filtered, so the high frequency region) faster to zero. 
+    (in the low-pass-filtered, so the high frequency region) faster to zero. Furthermore, the cutoff at the frequency of 48 MHz is 
+    significantly sharper for the filter of length 200. 
     '''
     h_50 = impulse_response(M=50)
     h_200 = impulse_response(M=200)
@@ -80,29 +59,21 @@ def main(signal = noise):
     plt.plot(h_200, label = 'M = 200')
     plt.legend()
     plt.show()
-    
-    # return None 
 
-    # convolve impulse function for filter of length 50 with noise
+    # convolve impulse response function for filter of length 50 with noise
     convolution_50 =  np.convolve(h_50, signal)
+    # and fourier transfrom the convoluted signal to the frequency domain
     convolution_f_50 = np.fft.rfft(convolution_50)
 
-    # convolve impulse function for filter of length 200 with noise
+    # convolve impulse response function for filter of length 200 with noise and fourier transform
     convolution_200 =  np.convolve(h_200, signal)
     convolution_f_200 = np.fft.rfft(convolution_200)
 
     # plot the noise in the frequency domain after applying the filter (convolving the impulse response with the noise and fourier transforming)
     plt.figure()
-<<<<<<< HEAD
-    plt.plot(convolution_f)
-    plt.title('Frequency spectrum of the filtered noise')
-=======
-    # x_f_50 = np.fft.rfftfreq(2*10**6+50)
-    # x_f_200 = np.fft.rfftfreq(2*10**6+200)
     plt.plot(convolution_f_50, label = 'M = 50', alpha = 0.6, color = 'green')
     plt.plot(convolution_f_200, label = 'M = 200', alpha = 0.6, color = 'blue')
     plt.title('Frequency spectrum of the filtered noise for two filter lenghts')
->>>>>>> 9a4ff93fa45237f122ad0571d9db8e542d407232
     plt.xlabel('Frequency')
     plt.ylabel('Amplitude')
     plt.legend() 
@@ -114,13 +85,9 @@ main()
 
 
 '''
-np.fft.rfftfreq() is the fuction you need für die Grenzen
+For an ideal filter, we would expect a sharp cut at the cutoff frequency of 48MHz. 
+For the filter of length 200, we see a more or less sharp cut at this cutoff frequency, and some further oscillation that decreases in amplitude. 
+The filtered signal where we used the filter of length 50 shows a rather broad descent starting at around 44MHz. The following converging oscillation
+has a significantly lower frequency compared to the signal filtered with a filter of length 200, thus converging slower and we see a higher amount of frequencies
+larger than the cutoff frequency still being part of the signal. 
 '''
-
-
-
-
-
-# Make a Fourier transform of your filtered noise and produce a plot of the frequency spectrum
-
-# Make the same plot for a filter of length M=200 and discuss the results.
